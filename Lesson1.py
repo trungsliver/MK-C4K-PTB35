ad4cefce1ee562169c625e39d8e87d6ffebdde1d
Bui Duc Trung <buiductrung.cva@gmail.com>
2025-11-02 16:03:00 +0700

    Lesson 1

---
+++ Lesson1.py
@@ -0,0 +1,36 @@
+# In / hiển thị dữ liệu ra màn hình
+print("Hello World!")
+
+# Variables - Biến số
+    # Dùng để lưu trữ dữ liệu
+    # Có thể thay đổi được trong khi lập trình
+name = "Duc Trung"
+age = 2
+
+# 4 cách hiển thị dữ liệu ra màn hình
+    # cách 1: Dùng dấu +
+print("Họ tên: " + name)
+    # cách 2: Dùng dấu ,
+print("Tuổi:", age)
+print("Tôi tên là", name, ". Tôi", age, "tuổi.")
+    # cách 3: Dùng f-string (truyền dữ liệu vào văn bản)
+print(f"Tôi tên là {name}. Tôi {age} tuổi.")
+    # cách 4: Hiển thị nhiều dòng
+print("""
+Đây là dòng thứ 1
+Đây là dòng thứ 2
+Đây là dòng thứ 3
+""")
+
+# Các phép toán cơ bản
+    #  Cơ bản: + - * /
+print("1 + 1 =", 1 + 1)
+print("5 - 2 =", 5 - 2)
+print("2 * 3 =", 2 * 3)
+print("8 / 4 =", 8 / 4)
+    # Chia lấy nguyên: //
+print("7 // 2 =", 7 // 2)
+    # Chia lấy dư: %
+print("7 % 2 =", 7 % 2)
+    # Lũy thừa: **
+print("2 ** 3 =", 2 ** 3)
