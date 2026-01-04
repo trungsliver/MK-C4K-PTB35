7be771848cdb53db10b436fcddbf073bc71a18b1
Bui Duc Trung <buiductrung.cva@gmail.com>
2026-01-04 14:00:18 +0700

    Lesson 10

---
+++ Lesson10.py
@@ -0,0 +1,18 @@
+# Bài 1: Viết chương trình nhập từ bàn phím danh sách a. Hãy trả về kết quả các phần tử trong danh sách theo thứ tự tăng dần.
+
+# Bài 2: Viết chương trình nhập từ bàn phím danh sách a. Hãy tìm ra phần tử lớn nhất và nhỏ nhất từ danh sách a và trả về kết quả.
+
+# Bài 3: Viết chương trình nhập từ bàn phím danh sách a. Hãy tính giá trị trung bình của các phần tử trong danh sách a và trả về kết quả giá trị trung bình.
+
+# Bài 4: Viết chương trình nhập từ bàn phím danh sách a. Tính tổng các số lẻ và tổng các số chẵn trong danh sách a.
+
+# Bài 5: Viết chương trình khai báo sẵn danh sách a. Viết chương trình bao gồm các chức năng: hiện toàn bộ phần tử danh sách, thêm phần tử vào danh sách, sửa phần tử danh sách, xóa phần tử trong danh sách.
+
+# Bài 6: 
+# YC1: Nhập vào từ bàn phím 1 danh sách bao gồm 10 số nguyên
+# YC2: Thêm số '-5' vào vị trí thứ 2 (index=2) trong danh sách
+# YC3: Tính tổng các số chẵn trong danh sách
+# YC4: Đếm số lượng số lẻ có trong danh sách
+# YC5: Tìm giá trị phần tử lớn nhất của danh sách
+# YC6: Tìm vị trí phần tử nhỏ nhất của danh sách
+# YC7: Tính giá trị trung bình của các phần tử trong danh sách
